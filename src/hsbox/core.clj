a487e9b8d48b01955dbda7281d437bc0f3f0286c
bugdone <bugdone@gmail.com>
2015-05-03 11:15:59 +0300

    more logging

--- src/hsbox/core.clj
+++ src/hsbox/core.clj
@@ -14,21 +14,23 @@
 (timbre/refer-timbre)
 
 (defn -main [& args]
-  (let [port (try
-               (Integer/parseInt (nth args 0))
-               (catch Exception e 4000))
-        server (ring.adapter.jetty/run-jetty #'app {:port port :join? false})]
-    (info "HeadshotBox " (version/get-version))
-    (future (version/update-latest-version-every-day))
-    (.start server)
-    (db/init-db-if-absent)
-    (stats/init-cache)
-    (indexer/set-indexed-path (db/get-demo-directory))
-    (indexer/set-indexing-state true)
-    (db/keep-only (->> (db/get-demo-directory)
-                       (clojure.java.io/as-file)
-                       file-seq
-                       (map #(.getName %))))
-    (stats/init-cache)
-    (indexer/add-demo-directory (db/get-demo-directory))
-    (indexer/run)))
+  (try
+    (let [port (try
+                 (Integer/parseInt (nth args 0))
+                 (catch Exception e 4000))
+          server (ring.adapter.jetty/run-jetty #'app {:port port :join? false})]
+      (info "HeadshotBox " (version/get-version))
+      (future (version/update-latest-version-every-day))
+      (.start server)
+      (db/init-db-if-absent)
+      (stats/init-cache)
+      (indexer/set-indexed-path (db/get-demo-directory))
+      (indexer/set-indexing-state true)
+      (db/keep-only (->> (db/get-demo-directory)
+                         (clojure.java.io/as-file)
+                         file-seq
+                         (map #(.getName %))))
+      (stats/init-cache)
+      (indexer/add-demo-directory (db/get-demo-directory))
+    (indexer/run))
+    (catch Exception e (error e))))
