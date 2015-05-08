0e511948bf1fe4170051979768471017877c6636
bugdone <bugdone@gmail.com>
2015-05-08 21:16:54 +0300

    Fix indexer dying when the demo dir doesn't exist.

--- src/hsbox/util.clj
+++ src/hsbox/util.clj
@@ -4,6 +4,9 @@
   (let [file (clojure.java.io/as-file path)]
     (and (.exists file) (not (.isDirectory file)))))
 
+(defn path-exists? [path]
+  (let [p (clojure.java.io/as-file path)]
+    (.exists p)))
 
 (defn current-timestamp []
   (quot (System/currentTimeMillis) 1000))
