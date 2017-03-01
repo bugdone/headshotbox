efc7e9ab042a211a69aa692417a62b12e5079f4a
bugdone <bugdone@gmail.com>
2017-03-01 22:55:53 +0200

    build: retry getting demoinfogo binaries if fail

--- get-demoinfogo.py
+++ get-demoinfogo.py
@@ -1,8 +1,15 @@
 import urllib
 import json
 import os
+import traceback
 
-demoinfogo = urllib.urlopen('https://api.github.com/repos/bugdone/demoinfogo-linux/releases').read()
-assets = json.loads(demoinfogo)[0]['assets']
-for url in (assets[0]['browser_download_url'], assets[1]['browser_download_url']):
-    os.system('wget ' + url)
+
+for i in range(3):
+    try:
+        demoinfogo = urllib.urlopen('https://api.github.com/repos/bugdone/demoinfogo-linux/releases').read()
+        assets = json.loads(demoinfogo)[0]['assets']
+        for url in (assets[0]['browser_download_url'], assets[1]['browser_download_url']):
+            os.system('wget ' + url)
+        break
+    except Exception as e:
+        traceback.print_exc()
