863f3f4319d86911a4cfc2b0d7ab5894ec377897
bugdone <bugdone@gmail.com>
2017-03-02 10:53:32 +0200

    print github api result

--- get-demoinfogo.py
+++ get-demoinfogo.py
@@ -7,6 +7,7 @@ import traceback
 for i in range(3):
     try:
         demoinfogo = urllib.urlopen('https://api.github.com/repos/bugdone/demoinfogo-linux/releases').read()
+        print demoinfogo
         assets = json.loads(demoinfogo)[0]['assets']
         for url in (assets[0]['browser_download_url'], assets[1]['browser_download_url']):
             os.system('wget ' + url)
