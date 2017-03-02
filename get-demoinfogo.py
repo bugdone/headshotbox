d497cc17b37063d71d398985c4838dd130ccbbbf
bugdone <bugdone@gmail.com>
2017-03-02 12:55:08 +0200

    build: use github api token to work around the unauthenticated api limits

--- get-demoinfogo.py
+++ get-demoinfogo.py
@@ -1,14 +1,19 @@
-import urllib
 import json
 import os
 import traceback
 
+import requests
+
 
 for i in range(3):
     try:
-        demoinfogo = urllib.urlopen('https://api.github.com/repos/bugdone/demoinfogo-linux/releases').read()
-        print demoinfogo
-        assets = json.loads(demoinfogo)[0]['assets']
+        url = 'https://api.github.com/repos/bugdone/demoinfogo-linux/releases'
+        auth = None
+        if os.environ.get('GITHUB_TOKEN'):
+            auth = (os.environ['GITHUB_USER'], os.environ['GITHUB_TOKEN'])
+        demoinfogo = requests.get(url, auth=auth)
+        print demoinfogo.status_code, demoinfogo.headers, demoinfogo.content
+        assets = json.loads(demoinfogo.content)[0]['assets']
         for url in (assets[0]['browser_download_url'], assets[1]['browser_download_url']):
             os.system('wget ' + url)
         break
