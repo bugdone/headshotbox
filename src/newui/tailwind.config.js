834218b2584c6c73473e590b7f5afd0f01629b17
Marius Palade <marius.palade@qualitance.com>
2023-05-20 08:29:28 +0300

    add Montserrat font and tailwind support

---
+++ src/newui/tailwind.config.js
@@ -0,0 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
