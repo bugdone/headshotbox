d65b7edff3e217bc043ca5801576b203f993cc9a
Andrei Benea <abenea@gmail.com>
2023-09-23 12:40:49 +0200

    Add 'tracing' feature for debugging.

--- csdemoparser/src/main.rs
+++ csdemoparser/src/main.rs
@@ -1,6 +1,17 @@
 use std::{env, error, fs::File};
 
+#[cfg(feature = "tracing")]
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+
 fn main() -> Result<(), Box<dyn error::Error>> {
+    #[cfg(feature = "tracing")]
+    {
+        tracing_subscriber::registry()
+            .with(fmt::layer().without_time())
+            .with(EnvFilter::from_default_env())
+            .init();
+    }
+
     let mut args = env::args();
     args.next();
     let dem_path = args.next().ok_or("need dem file path")?;
